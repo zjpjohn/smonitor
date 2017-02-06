4b8aa2aee06075fa8bed5c50b523695a59269763
harlan <emailhl@163.com>
2017-02-06 16:48:17 +0800

    modules with properties need notice

--- notice/notice-email/src/main/java/com/harlan/smonitor/notice/email/EmailNoticeService.java
+++ notice/notice-email/src/main/java/com/harlan/smonitor/notice/email/EmailNoticeService.java
@@ -7,6 +7,8 @@ import com.harlan.smonitor.api.notice.INoticeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ResourceBundle;
+
 /**
  * 发送邮件实现类
  * Created by harlan on 2016/11/22.
@@ -17,6 +19,15 @@ public class EmailNoticeService implements INoticeService {
     public Result sendMessage(Admin admin, String title, String content) {
         EmailAdmin emailAdmin= (EmailAdmin) admin;
         logger.info("发送邮件，邮箱为：{},标题为：{},内容为：{}",emailAdmin.getEmailAddress(),title,content);
+        try {
+            ResourceBundle rb = ResourceBundle.getBundle("notice-email");
+            String server=rb .getString("email_server");
+
+            logger.info("email_server:"+server);
+        } catch (Exception e) {
+            logger.error("发送邮件异常",e);
+        }
+
         return new Result();
     }
 
